ca0ce5fa500c851b2ed024988c6175e832604838
jolebowski <jordan.moreira@epitech.eu>
2022-06-29 19:23:15 +0200

    add a new state for filter monster array

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import "./App.css";
 class App extends Component {
   constructor() {
     super();
+
     this.state = {
       monsters: [],
+      monstersfilterInit: [],
+      value: "",
     };
-    console.log("in constructor");
+    this.handleChange = this.handleChange.bind(this);
+    console.log("in constructor 1");
   }
 
   componentDidMount() {
-    console.log("in compoenentDidMount");
+    console.log("in compoenentDidMount 3");
     fetch("https://jsonplaceholder.typicode.com/users")
       .then(
         (res) => res.json() // Nous convertissons en format JSON pour mieux recupérer les éléments dans un format adapté
@@ -20,20 +24,53 @@ class App extends Component {
       .then((users) => this.setState({ monsters: users }))
       .catch((e) => console.log(e));
   }
+  handleChange(e) {
+    this.setState({
+      value: e.target.value,
+    });
+    const monsterFilter = this.state.monsters.filter((monster) =>
+      monster.name
+        .toLocaleUpperCase()
+        .includes(this.state.value.toLocaleUpperCase("fr-FR"))
+    );
+    this.setState({
+      monstersfilterInit: monsterFilter,
+    });
+    console.log(monsterFilter, "monsterFilter");
+  }
 
   render() {
     {
-      console.log("in render");
+      console.log("in render 2 ");
+      console.log(
+        this.state.monstersfilterInit.length > 0,
+        "this.state.monstersfilterInit.length > 0"
+      );
     }
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
-          return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1>
-            </div>
-          );
-        })}
+        <input
+          type="search"
+          name="monster"
+          value={this.state.value}
+          placeholder="Search monsters by name"
+          onChange={this.handleChange}
+        />
+        {this.state.monstersfilterInit.length > 0
+          ? this.state.monstersfilterInit.map((monster) => {
+              return (
+                <div key={monster.id}>
+                  <h1>{monster.name}</h1>
+                </div>
+              );
+            })
+          : this.state.monsters.map((monster) => {
+              return (
+                <div key={monster.id}>
+                  <h1>{monster.name}</h1>
+                </div>
+              );
+            })}
       </div>
     );
   }
