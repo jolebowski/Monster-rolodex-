2809b13112e691f16a3313ce2a462f32565db6ba
jolebowski <jordan.moreira@epitech.eu>
2022-07-01 13:00:01 +0200

    add a new font and add a card component

--- src/App.js
+++ src/App.js
@@ -44,6 +44,7 @@ class App extends Component {
 
     return (
       <div>
+        <h1 className="app-title">Monster rolodex</h1>
         <SearchBox onChangeHandler={handleChange} />
         <CardList monsterFilter={monsterFilter} />
       </div>
--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,25 +1,13 @@
 import { Component } from "react";
-
+import Card from "../Card/Card";
 import "./card-container.css";
-import "./card.css";
-
 export default class CardList extends Component {
   render() {
     const { monsterFilter } = this.props;
     return (
       <div className="card-container">
         {monsterFilter.map((monster) => {
-          const { name, id, email } = monster;
-          return (
-            <div className="card">
-              <img
-                alt={`monster ${name}`}
-                src={`https://robohash.org/${id}?set=set2`}
-              />
-              <h1>{name}</h1>
-              <p>{email}</p>
-            </div>
-          );
+          return <Card monster={monster} />;
         })}
       </div>
     );
