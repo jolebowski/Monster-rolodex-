fa82e0782136ac3f9795c009156ebea466126e37
jolebowski <jordan.moreira@epitech.eu>
2022-07-13 16:40:49 +0200

    transform all class components to function components

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,15 +1,13 @@
-import { Component } from "react";
 import Card from "../Card/Card";
 import "./card-container.css";
-export default class CardList extends Component {
-  render() {
-    const { monsterFilter } = this.props;
-    return (
-      <div className="card-container">
-        {monsterFilter.map((monster) => {
-          return <Card key={monster.id} monster={monster} />;
-        })}
-      </div>
-    );
-  }
-}
+
+const CardList = ({ monsterFilter }) => (
+  <div className="card-container">
+    {monsterFilter.map((monster) => {
+      console.log(monster, "moinster");
+      return <Card key={monster.id} monster={monster} />;
+    })}
+  </div>
+);
+
+export default CardList;
--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -1,18 +1,16 @@
-import { Component } from "react";
 import "./searchBox.css";
-export default class SearchBox extends Component {
-  render() {
-    const { onChangeHandler } = this.props;
-    return (
-      <>
-        <input
-          className="search-box"
-          type="search"
-          name="monster"
-          placeholder="Search monsters by name"
-          onChange={onChangeHandler}
-        />
-      </>
-    );
-  }
-}
+
+const SearchBox = ({ onChangeHandler }) => {
+  return (
+    <>
+      <input
+        className="search-box"
+        type="search"
+        name="monster"
+        placeholder="Search monsters by name"
+        onChange={onChangeHandler}
+      />
+    </>
+  );
+};
+export default SearchBox;
